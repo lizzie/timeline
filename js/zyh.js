e3d14fea42da63e2d3e6716624093a94f1adea52
liz <shengyan1985@gmail.com>
2012-11-09 16:44:38 +0800

    add zyh's data

---
+++ js/zyh.js
@@ -0,0 +1,187 @@
+/**
+ * new Date(year, month, day [, hour, minute, second, millisecond])
+ */
+define({
+    "unit": [
+        {
+            "id": 1,
+            "title": "小星星处女照",
+            "img_url": "http://ww3.sinaimg.cn/large/7745c6b3jw1dsoc80fqjuj.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 5, 6)
+        },
+        {
+            "id": 2,
+            "title": "小星星每天换个样子，今天是第19天，刚吃完奶，美美睡一觉",
+            "img_url": "http://ww1.sinaimg.cn/mw690/7745c6b3jw1dsuhtbs5c1j.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 5, 11)
+        },
+        {
+            "id": 3,
+            "title": "这个姿势牛吧",
+            "img_url": "http://ww1.sinaimg.cn/mw690/7745c6b3jw1dt0a4akbu6j.jpg",
+            "img_width": 640,
+            "img_height": 479,
+            "desc": "",
+            "datetime": new Date(2012, 5, 16)
+        },
+        {
+            "id": 4,
+            "title": "两个月差9天",
+            "img_url": "https://lh6.googleusercontent.com/-kk6BQ3dEUP4/T9h2R37tToI/AAAAAAAAAII/bEQqf6J6Ukg/s447/IMAG0126.jpg",
+            "img_width": 641,
+            "img_height": 851,
+            "desc": "",
+            "datetime": new Date(2012, 6, 13)
+        },
+        {
+            "id": 5,
+            "title": "6月16号玉照",
+            "img_url": "http://ww1.sinaimg.cn/mw690/7745c6b3jw1du18teyrjaj.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 6, 16)
+        },
+        {
+            "id": 6,
+            "title": "可爱吧",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1duipy6rjurj.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 7, 2)
+        },
+        {
+            "id": 7,
+            "title": "我越来越可爱了，大家快来亲亲我吧",
+            "img_url": "http://ww3.sinaimg.cn/mw690/7745c6b3jw1duipztrfkdj.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 7, 2)
+        },
+        {
+            "id": 8,
+            "title": "妈妈爱你…",
+            "img_url": "http://ww3.sinaimg.cn/mw690/7745c6b3jw1duix2452rqj.jpg",
+            "img_width": 640,
+            "img_height": 896,
+            "desc": "",
+            "datetime": new Date(2012, 7, 2)
+        },
+        {
+            "id": 9,
+            "title": "越来越像我了哈哈",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1dup7yj0j4vj.jpg",
+            "img_width": 640,
+            "img_height": 479,
+            "desc": "",
+            "datetime": new Date(2012, 7, 8)
+        },
+        {
+            "id": 10,
+            "title": "星星趴着的玉照，今天竟然从趴着自己翻到仰睡，可惜没摄影下来。",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1duvnwsgw50j.jpg",
+            "img_width": 640,
+            "img_height": 479,
+            "desc": "",
+            "datetime": new Date(2012, 7, 14)
+        },
+        {
+            "id": 11,
+            "title": "我换发型了，换了更可爱是不是…",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1dv2grjll2ij.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 7, 19)
+        },
+        {
+            "id": 12,
+            "title": "@帆帆狼",
+            "img_url": "http://ww4.sinaimg.cn/mw690/70b64a70gw1dw1vapfuukj.jpg",
+            "img_width": 690,
+            "img_height": 1036,
+            "desc": "",
+            "datetime": new Date(2012, 8, 19)
+        },
+        {
+            "id": 13,
+            "title": "ohlife 2012-08-15",
+            "img_url": "https://s3.amazonaws.com/ohlife/photos/d5b4b35e6d483ce901e07da8e083fe0e/fb1db1480e2247fe7103348841705114.jpg",
+            "img_width": 717,
+            "img_height": 960,
+            "desc": "",
+            "datetime": new Date(2012, 8, 15)
+        },
+        {
+            "id": 14,
+            "title": "ohlife 2012-08-25",
+            "img_url": "https://s3.amazonaws.com/ohlife/photos/d5b4b35e6d483ce901e07da8e083fe0e/0f93d16b30c075e63abaf536dd86f61b.jpeg",
+            "img_width": 5616,
+            "img_height": 3744,
+            "desc": "",
+            "datetime": new Date(2012, 8, 25)
+        },
+        {
+            "id": 15,
+            "title": "因为有你我才快乐",
+            "img_url": "http://ww1.sinaimg.cn/mw690/7745c6b3jw1dwq9zi11n0j.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 9, 9)
+        },
+        {
+            "id": 16,
+            "title": "我的回头率可高了",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1dxiclf1x47j.jpg",
+            "img_width": 640,
+            "img_height": 884,
+            "desc": "at乌镇",
+            "datetime": new Date(2012, 10, 3)
+        },
+        {
+            "id": 17,
+            "title": "很喜欢这个学步车",
+            "img_url": "http://ww4.sinaimg.cn/mw690/7745c6b3jw1dyb83o1zc6j.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 10, 28)
+        },
+        {
+            "id": 18,
+            "title": "在孩子眼里什么都是好玩的，就我吸奶机上的一根线就能玩半天",
+            "img_url": "http://ww3.sinaimg.cn/mw690/7745c6b3jw1dycg7kfccij.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 10, 29)
+        },
+        {
+            "id": 19,
+            "title": "星星自己能站了",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1dydgnneyyej.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 10, 30)
+        },
+        {
+            "id": 20,
+            "title": "星星自己能站了",
+            "img_url": "http://ww2.sinaimg.cn/mw690/7745c6b3jw1dydgnneyyej.jpg",
+            "img_width": 640,
+            "img_height": 857,
+            "desc": "",
+            "datetime": new Date(2012, 10, 30)
+        }
+    ]
+});
